2961988d8cf1ce6a591cb0914ba1fc1c91f4586b
Vijay Kumar Jalagari <jalagari@adobe.com>
2019-11-03 23:27:22 +0530

    Adding HOST details in docker using env

--- sample-web-node/server.js
+++ sample-web-node/server.js
@@ -1,23 +1,28 @@
 var http = require("http");
 var fs = require('fs');
 var url = require('url');
+var podname= process.env.HOSTNAME;
+var requests = 0;
+var startTime = new Date();
 
 var handleRequest = function(req, res) {
   var q = url.parse(req.url, true);
   var fileName = q.pathname.replace("/","") + ".html";
   var parameters = q.query;
-  console.log("Reading file " + fileName)
+  console.log("Reading file " + fileName);
   fs.readFile(fileName, function(err, data){
 	data = data || "Page not found and we are working on it";
 	res.writeHead(200, {'Content-Type': 'text/html',
-			    'Content-Length': data.length});  	
+			    'Content-Length': data.length});  
 	if(err) {
 		console.log("Error in read file " + fileName);
 	} else {
 		console.log("Found file and sending data");
-	}
-		res.write(data);
-		return res.end();
+	}	
+	res.write("Serving response from HOST " + podname);
+	res.write(data);
+	console.log("Running On:" ,podname, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());
+	return res.end();
   });
 };
 
